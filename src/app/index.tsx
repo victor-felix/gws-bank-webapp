3452844fe23010dd3451e5d93f8632a4337da350
Victor Felix <victorfelix.dev@gmail.com>
2024-01-16 13:22:49 -0300

    feat: Creating gws bank webapp

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,30 +9,59 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import { GlobalStyle } from 'styles/global-styles';
-
-import { HomePage } from './pages/HomePage/Loadable';
-import { NotFoundPage } from './pages/NotFoundPage/Loadable';
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from '@mui/material/styles';
+import { PaletteMode } from '@mui/material';
+import { indigo } from '@mui/material/colors';
+import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
+import { GlobalStyle } from 'styles/global-styles';
+import { NotFoundPage } from './components/NotFoundPage/Loadable';
+import { AccountPage } from './pages/AccountPage';
+import { selectMode } from './components/ToggleTheme/slice/selectors';
+
 export function App() {
+  const mode = useSelector(selectMode);
+
+  let theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: mode as PaletteMode,
+          primary: {
+            main: indigo[500],
+          },
+        },
+        typography: {
+          fontFamily: ['Raleway Variable', 'sans-serif'].join(','),
+        },
+      }),
+    [mode],
+  );
+
+  theme = responsiveFontSizes(theme);
+
   const { i18n } = useTranslation();
   return (
-    <BrowserRouter>
-      <Helmet
-        titleTemplate="%s - React Boilerplate"
-        defaultTitle="React Boilerplate"
-        htmlAttributes={{ lang: i18n.language }}
-      >
-        <meta name="description" content="A React Boilerplate application" />
-      </Helmet>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      <GlobalStyle />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Helmet
+          titleTemplate="%s - React Boilerplate"
+          defaultTitle="React Boilerplate"
+          htmlAttributes={{ lang: i18n.language }}
+        >
+          <meta name="description" content="A React Boilerplate application" />
+        </Helmet>
+        <Routes>
+          <Route path="/" element={<AccountPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <GlobalStyle theme={mode} />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,9 @@
 import { RootState } from './RootState';
 
 export type { RootState };
+
+export interface ApiResponseError {
+  message: string;
+  error: string;
+  statusCode: 0;
+}
